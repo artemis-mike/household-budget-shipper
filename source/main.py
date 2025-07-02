86cda41b512807362cf096bbad724d456336b009
Michael <>
2025-07-03 01:15:28 +0200

    Define variables and download file

--- source/main.py
+++ source/main.py
@@ -1,10 +1,15 @@
 import logging, os, requests, json, time, signal, sys
 from datetime import datetime
+from requests.auth import HTTPBasicAuth
 
 # VARIABLE   = os.environ.get("VARIABLE", None)
-LOGLEVEL  = os.environ.get("SHIPPER_LOGLEVEL", "INFO")
-INTERVAL        = int(os.environ.get("SHIPPER_INTERVAL", 30))
-FORCE_INTERVAL  = os.environ.get("SHIPPER_FORCE_INTERVAL", False)
+NEXTCLOUD_BASE_URL  = os.environ.get("SHIPPER_NEXTCLOUD_BASE_URL").rstrip("/")
+FILEPATH_BUDGET_DIRECTORY = os.environ.get("SHIPPER_FILEPATH_BUDGET_DIRECTORY").rstrip("/").lstrip("/")
+NEXTCLOUD_USERNAME  = os.environ.get("SHIPPER_NEXTCLOUD_USERNAME")
+NEXTCLOUD_PASSWORD  = os.environ.get("SHIPPER_NEXTCLOUD_PASSWORD")
+LOGLEVEL            = os.environ.get("SHIPPER_LOGLEVEL", "INFO")
+INTERVAL            = int(os.environ.get("SHIPPER_INTERVAL", 30))
+FORCE_INTERVAL      = os.environ.get("SHIPPER_FORCE_INTERVAL", False)
 
 logging.basicConfig(format='%(asctime)s %(levelname)s\t%(message)s', encoding='utf-8')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -17,9 +22,11 @@ def signal_handler(sig, frame):
 def check_settings():
   error = False
   # Do some checks like
-  #  if (VARIABLE is None):
-  #    logging.critical("VARIABLE is not set. This is required. Exiting.")
-  #    error = True
+  requiredVars = ["NEXTCLOUD_BASE_URL", "FILEPATH_BUDGET_DIRECTORY", "NEXTCLOUD_USERNAME", "NEXTCLOUD_PASSWORD", "LOGLEVEL"]
+  for var in requiredVars:
+    if (globals().get(var) is None):
+        logging.critical(f"SHIPPER_{var} is not set. This is required. Exiting.")
+        error = True
   if (INTERVAL < 5 and FORCE_INTERVAL == False):
     logging.info("It's not recommended to set SHIPPER_INTERVAL to a value less than 5. Your value is %s. Default value is 30.", INTERVAL)
     logging.info("Set SHIPPER_FORCE_INTERVAL = True to ignore this warning.")
@@ -29,13 +36,32 @@ def check_settings():
   else:
     return 0
   
+def getCurrentYear():
+  return datetime.date.today().year
+
+def downloadFile(filename, path="workbench/"):
+  remoteFullPath = NEXTCLOUD_BASE_URL +"/remote.php/dav/files/"+ NEXTCLOUD_USERNAME +"/"+ FILEPATH_BUDGET_DIRECTORY +"/"+ filename
+  response = requests.get(remoteFullPath, auth=HTTPBasicAuth(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD), stream=True)
+  if response.status_code == 200:
+    with open(path+filename, "wb") as f:
+      for chunk in response.iter_content(chunk_size=8192):
+        if chunk:
+          f.write(chunk)
+    logging.info(f"Downloaded file {filename} successfully.")
+  else:
+    logging.error(f"Download failed with status code: {response.status_code}")
+  
 
 def main():
   logging.debug("Running with this configuration:")
+  if (check_settings() != 0):
+    logging.error("Failed settings check. Exit.")
+    return 1
   # Do some stuff
 
   while(True):
     # Main control loop goes in here
+    downloadFile("template.ods")
     logging.info("Nothing to do. Sleeping for %ss.", INTERVAL)
     time.sleep(INTERVAL)
 
